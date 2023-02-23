613438756a8fe4fcab8dd45109215932593bf0ee
yunpeng.li <yunpeng.li@vaneplus.com>
2023-02-23 18:36:35 +0800

    #202302231836 add shell test_installation

---
+++ test_installation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+go install ./cmd/goass.go
+
+rm ./resources/example_test.go
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc1
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc1
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc2
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc3
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=OneTemplateFunc -types=string
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=DoubleSameTemplateFunc -types=reflect.SliceHeader
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=DoubleDifferenceTemplateFunc -types=string,reflect.StringHeader
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=TypeConstraintsTemplateFunc -types=int8
+
+sed -i 's/testing\.T/T\.testing/g' ./resources/example_test.go
+
+grep 'T\.testing' ./resources/example_test.go | wc -l
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc1
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc1
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc2
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=ExampleFunc3
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=OneTemplateFunc -types=string
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=DoubleSameTemplateFunc -types=reflect.SliceHeader
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=DoubleDifferenceTemplateFunc -types=string,reflect.StringHeader
+
+goass -cmd=generate -opt=unittest -file=./resources/example.go -func=TypeConstraintsTemplateFunc -types=int8
